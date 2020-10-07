c450c77a408a930a3a2e3b9ee1e527c47e88d16e
b1team <b1@b1.com>
2020-10-07 10:42:30 +0700

    🔥 feat/add_job_form

--- components/AddJobForm.js
+++ components/AddJobForm.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, TextInput } from 'react-native';
-import { Item, Input, Button, Text, Form, Textarea, Icon } from 'native-base';
+import { Item, Input, Button, Text, Form, Textarea, Icon, List } from 'native-base';
 import Drawer from './Drawer';
 import styles from '../Style/Styles';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 
 function Numberic() {
@@ -17,11 +18,11 @@ function Numberic() {
     );
 }
 
-function Description(){
-    return(
+function Description() {
+    return (
         <Form>
-        <Textarea rowSpan={5} bordered placeholder="Desciption"/>
-      </Form>
+            <Textarea rowSpan={5} bordered placeholder="Desciption" />
+        </Form>
     );
 }
 
@@ -39,21 +40,25 @@ function Buttons() {
 };
 
 function AddJobForm({ navigation }) {
+    const [emails_input, setEmails] = useState(['tungnt', 'ble']);
+    const [text_input, setText] = useState('');
     return (
         <View style={{ flex: 1 }}>
             <Drawer navigation={navigation} name={"Thêm việc"} />
-            <Item style={styles.job_name}>
-                <Input placeholder='Tên công việc' style={styles.input} />
-            </Item>
-            <Numberic />
-            <Item style={styles.address}>
-                <Input placeholder='Địa chỉ' style={styles.input} />
-            </Item>
-            <Description/>
-            <Item style={styles.address}>
-                <Input placeholder='Gán thẻ' style={styles.input} />
-            </Item>
-            <Buttons/>
+            <KeyboardAwareScrollView style={{ flex: 1 }}>
+                <Item style={styles.job_name}>
+                    <Input placeholder='Tên công việc' style={styles.input} />
+                </Item>
+                <Numberic />
+                <Item style={styles.address}>
+                    <Input placeholder='Địa chỉ' style={styles.input} />
+                </Item>
+                <Description />
+                <Item style={styles.address}>
+                    <Input placeholder='Gán thẻ' style={styles.input} />
+                </Item>
+                <Buttons />
+            </KeyboardAwareScrollView>
         </View>
     );
 }
