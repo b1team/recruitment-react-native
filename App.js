df547542ed50b9b67458c88d0953fd99762af162
b1team <b1@b1.com>
2020-10-13 21:47:04 +0700

    add auth function

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
-} from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './components/Home';
-import { Icon } from 'native-base';
-import LoginForm from './components/LoginForm';
-import ProfileScreen from './components/Profile';
-
+} from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./components/Home";
+import { Icon, Text, View, Button } from "native-base";
+import LoginForm from "./components/LoginForm";
+import ProfileScreen from "./components/Profile";
+import { getToken, saveToken, logout } from "./Auth";
 
 function CustomDrawerContent(props) {
   return (
@@ -20,38 +20,88 @@ function CustomDrawerContent(props) {
   );
 }
 
+function testComponent({navigation, route}){
+  return (
+    <View>
+      <Text>Ahihi</Text>
+      <Button onPress={() => saveToken("Vuonglv", route.params.token.setter)}><Text>Login</Text></Button>
+    </View>
+  );
+}
+
+function LogOutScreen({navigation, route}) {
+  return (
+    <View>
+      <Text>Logout</Text>
+      <Button onPress={() => logout(route.params.token.setter)}><Text>Logout</Text></Button>
+    </View>
+  );
+}
+
 
 const Drawer = createDrawerNavigator();
 
 function App() {
+  const [token, setToken] = React.useState(null)
+  React.useEffect(() => {
+    getToken(setToken)
+  })
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home" drawerContent={props => <CustomDrawerContent {...props} />}>
-        <Drawer.Screen name="Home" component={HomeScreen} options={{
-          title: 'Trang chủ',
-          drawerIcon: () => (
-            <Icon
-              name="md-home"
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+      >
+        {console.log(token)}
+        {token ? (
+          <>
+            <Drawer.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                title: "Trang chủ",
+                drawerIcon: () => <Icon name="md-home" />,
+              }}
             />
-          ),
-        }} />
-
-        <Drawer.Screen name="Login" component={LoginForm} options={{
-          title: 'Đăng nhập',
-          drawerIcon: () => (
-            <Icon
-              name="ios-person"
+            <Drawer.Screen
+              name="Login"
+              component={LoginForm}
+              options={{
+                title: "Đăng nhập",
+                drawerIcon: () => <Icon name="ios-person" />,
+              }}
             />
-          ),
-        }} />
-        <Drawer.Screen name="Profile" component={ProfileScreen} options={{
-          title: 'Profile',
-          drawerIcon: () => (
-            <Icon
-              name="ios-person"
+            <Drawer.Screen
+              name="Profile"
+              component={ProfileScreen}
+              options={{
+                title: "Profile",
+                drawerIcon: () => <Icon name="ios-person" />,
+              }}
             />
-          ),
-        }} />
+            <Drawer.Screen
+              name="Logout"
+              component={LogOutScreen}
+              initialParams={{token: {setter: setToken}}}
+              options={{
+                title: "Logout",
+                drawerIcon: () => <Icon name="ios-person" />,
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <Drawer.Screen
+              name="Test"
+              component={testComponent}
+              initialParams={{token: {setter: setToken}}}
+              options={{
+                title: "Test",
+                drawerIcon: () => <Icon name="ios-person" />,
+              }}
+            />
+          </>
+        )}
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- Auth.js
+++ Auth.js
@@ -1,31 +1,43 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from "@react-native-community/async-storage";
 
-const TOKEN_FIELD_NAME = "_token"
+const TOKEN_FIELD_NAME = "_token";
 
 
-export async function saveToken(token) {
-    try {
-        await AsyncStorage.setItem(TOKEN_FIELD_NAME, value)
-    } catch (e) {
-        console.log("Save token error")
-    }
+export async function removeToken() {
+  try {
+    await AsyncStorage.removeItem(TOKEN_FIELD_NAME);
+  } catch (e) {
+    console.log("Remove token error" + e);
+  }
+}
+
+export async function getToken(setTokenFunction) {
+  try {
+    token = await AsyncStorage.getItem(TOKEN_FIELD_NAME);
+    setTokenFunction(token);
+  } catch (e) {
+    // error reading value
+    console.log("Get token error");
+  }
 }
 
 
-export async function getToken() {
-    token = null
-    try {
-        token = await AsyncStorage.getItem(TOKEN_FIELD_NAME)
-        return token
-    } catch (e) {
-        // error reading value
-        console.log("Get token error")
-        return null
-    }
+export async function setTokenState(setTokenFunction) {
+  getToken().then(setTokenFunction);
 }
 
 
-export async function getIsSignedIn(){
-    const token = await getToken()
-    return token !== null
+export async function logout(setTokenFunction) {
+  await removeToken()
+  await setTokenState(setTokenFunction)
+}
+
+
+export async function saveToken(token, setTokenFunction) {
+  try {
+    await AsyncStorage.setItem(TOKEN_FIELD_NAME, token);
+    await setTokenState(setTokenFunction)
+  } catch (e) {
+    console.log("Save token error" + e);
+  }
 }
