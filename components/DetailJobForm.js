910ecc7779953792f3465cc7fe35933893ddc57a
b1team <b1@b1.com>
2020-10-13 16:22:11 +0700

    🔥feat/api-home-form

--- components/DetailJobForm.js
+++ components/DetailJobForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, SafeAreaView } from "react-native";
+import { Alert, SafeAreaView } from "react-native";
 import {
   Card,
   CardItem,
@@ -10,14 +10,18 @@ import {
 } from "native-base";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
+
+var userType = "employee"
+
 function DetailJobForm(props) {
-  var detail = props.detail
+  var job = props.detail
+  var navigation = props.navigation
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Card transparent>
         <CardItem>
           <Body>
-            <Text>{detail.title}</Text>
+            <Text>{job.title}</Text>
           </Body>
         </CardItem>
       </Card>
@@ -25,36 +29,36 @@ function DetailJobForm(props) {
         <Content padder>
           <Card>
             <CardItem header bordered>
-            {detail.tags.map((tag, index) => {
-                    return (
-                        <Button rounded success key={index} style={{ marginLeft: 5, height: 40 }}>
-                            <Text style={{ fontSize: 14 }} numberOfLines={1}>{tag}</Text>
-                        </Button>
-                    );
-                })}
+              {job.tags.map((tag, index) => {
+                return (
+                  <Button rounded success key={index} style={{ marginLeft: 5, height: 40 }}>
+                    <Text style={{ fontSize: 14 }} numberOfLines={1}>{tag}</Text>
+                  </Button>
+                );
+              })}
             </CardItem>
             <CardItem bordered>
               <Body>
                 <Text>Lương: </Text>
-                <Text>{detail.salary}</Text>
+                <Text>{job.salary}</Text>
               </Body>
             </CardItem>
             <CardItem bordered>
               <Body>
                 <Text>Địa chỉ:</Text>
-                <Text>{detail.address}</Text>
+                <Text>{job.address}</Text>
               </Body>
             </CardItem>
             <CardItem bordered>
               <Body>
                 <Text>Mô tả:</Text>
-                <Text>{detail.description}</Text>
+                <Text>{job.description}</Text>
               </Body>
             </CardItem>
             <CardItem footer bordered>
-              <Button primary style={{ width: 250, marginLeft: 35 }}>
+              {userType == "employer" ? null : <Button primary style={{ width: 250, marginLeft: 35 }} onPress={() => {navigation.navigate("ApplyScreen", {job: job})}}>
                 <Text style={{ marginLeft: 70 }}>Ứng Tuyển</Text>
-              </Button>
+              </Button>}
             </CardItem>
           </Card>
         </Content>
