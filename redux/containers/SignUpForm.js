b3add75d8333d202b771c82a69f7d3622327c3a3
vuonglv <it.vuonglv@gmail.com>
2020-10-15 21:40:15 +0700

    🔥 swap email input and password to top

--- redux/containers/SignUpForm.js
+++ redux/containers/SignUpForm.js
@@ -10,6 +10,7 @@ import Loader from '../../components/LoaderScreen';
 const styles = StyleSheet.create({
     username: {
         width: 350,
+        marginTop: 10,
     },
     mobileNumber: {
         width: 350,
@@ -56,32 +57,33 @@ function SignUpForm({ navigation, state, dispatch }) {
     const [phone_number, setPhoneNumber] = useState("")
     return (
         <View style={{ flex: 1 }}>
-            {state?.signupRequesting ? <Loader/>: null}
+            {state?.signupRequesting ? <Loader /> : null}
             <KeyboardAwareScrollView style={{ flex: 1 }}>
                 <Body>
                     <MainLogo width={130} height={130} />
                     <View style={styles.formAlign}>
-                        <Item style={styles.username}>
-                            <Input placeholder='Tên người dùng' Icon style={styles.input} value={name} onChangeText={(n) => setName(n)} />
-                        </Item>
-                        <Item style={styles.mobileNumber}>
-                            <Input placeholder='Số điện thoại' Icon style={styles.input} value={phone_number} onChangeText={(p) => setPhoneNumber(p)} />
-                        </Item>
                         <Item style={styles.email}>
                             <Input placeholder='Email' style={styles.input} value={email} onChangeText={(e) => setEmail(e)} />
                         </Item>
                         <Item style={styles.password}>
                             <Input placeholder='Mật khẩu' secureTextEntry={true} style={styles.input} value={password} onChangeText={(p) => setPassword(p)} />
                         </Item>
+                        <Item style={styles.username}>
+                            <Input placeholder='Tên người dùng' Icon style={styles.input} value={name} onChangeText={(n) => setName(n)} />
+                        </Item>
+                        <Item style={styles.mobileNumber}>
+                            <Input placeholder='Số điện thoại' Icon style={styles.input} value={phone_number} onChangeText={(p) => setPhoneNumber(p)} />
+                        </Item>
+
                     </View>
-                    <Button primary style={styles.button} onPress={() => {signupButtonHandler(dispatch, email, password, name, phone_number)}}>
+                    <Button primary style={styles.button} onPress={() => { signupButtonHandler(dispatch, email, password, name, phone_number) }}>
                         <Text style={{ fontSize: 16 }}>Đăng ký</Text>
                     </Button>
                     <Text style={{ marginTop: 15 }}>Đã có tài khoản?
                         <Text style={{ color: 'blue' }} onPress={() => navigation.navigate("LoginForm")}> Đăng nhập</Text>
                     </Text>
-                    {state?.error ? <Text style={{color: "red"}}>{state?.errorMessage}</Text> : null}
-                    {state?.signupDone ? <Text style={{color: "green"}}>Đăng ký thành công</Text> : null}
+                    {state?.error ? <Text style={{ color: "red" }}>{state?.errorMessage}</Text> : null}
+                    {state?.signupDone ? <Text style={{ color: "green" }}>Đăng ký thành công</Text> : null}
                 </Body>
             </KeyboardAwareScrollView>
         </View>
