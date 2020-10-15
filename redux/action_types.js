0af8d43f8a080ae8a7b015c707ea5cb90d6224f0
b1team <b1@b1.com>
2020-10-15 23:12:34 +0700

    fix action_types

--- redux/action_types.js
+++ redux/action_types.js
@@ -1,8 +1,7 @@
-export const LOGIN = 'LOGIN'
+export const LOGIN_REQUEST = 'LOGIN_REQUEST'
+export const LOGIN_ERROR = "LOGIN_ERROR"
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
 export const LOGOUT = 'LOGOUT'
-
-
-
 
 export const APPROVE_APPLY = 'APPROVE'
 export const REJECT_APPLY = 'REJECT'
